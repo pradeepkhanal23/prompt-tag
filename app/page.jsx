a473e6359ad9e88df1a60731b11a9dd518e461ad
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-08-21 16:31:50 +1000

    navbar issue fixed

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ const Home = () => {
     <section className="flex flex-col  items-center max-w-4xl px-5 py-10 rounded-lg mt-10 w-[90%] mx-auto text-center ">
       <div className="text-black ">
         <h1 className="text-5xl font-bold secondary_text ">Discover & Share</h1>
-        <p className="primary_text text-5xl font-bold">AI-powered </p>
-        <span className="primary_text text-5xl font-bold">Prompts</span>
+        <p className="primary_text text-5xl font-bold">Ideas &</p>
+        <p className="primary_text text-5xl font-bold">Prompts</p>
       </div>
       <p className="text-xl para_text mt-5 ">
         Hashtag is an open-source AI prompting tool for modern world to
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -29,88 +29,90 @@ const Navbar = () => {
             className="object-contain"
           />
         </Link>
-        <span className=" text-3xl secondary_text  uppercase font-semibold hidden md:block ">
+        <span className=" text-3xl secondary_text  uppercase font-semibold hidden md:flex ">
           hashtag
         </span>
       </div>
 
       {/* Desktop view */}
+      <div className="hidden sm:flex items-center  space-x-4 cursor-pointer">
+        {session?.user ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link
+              href="/create-prompt"
+              className="text-white primary_bg px-4 py-2 rounded-lg"
+            >
+              Create Post
+            </Link>
 
-      {session?.user ? (
-        <div className="hidden md:flex items-center  space-x-4 cursor-pointer">
-          <Link
-            href="/create-prompt"
-            className="text-white primary_bg px-4 py-2 rounded-lg"
-          >
-            Create Post
-          </Link>
-          <button
-            className="text-black btn border-2 border-black"
-            type="button"
-            onClick={signOut}
-          >
-            Sign Out
-          </button>
+            <button
+              type="button"
+              onClick={signOut}
+              className="text-black btn border-2 border-black"
+            >
+              Sign Out
+            </button>
 
-          <Link href="/profile">
-            <Image
-              src={session?.user.image}
-              alt="profile-image"
-              width={50}
-              height={50}
-              className="h-11 w-11 rounded-full border-[#2b6cb0] border-2 p-0.5 "
-            />
-          </Link>
-        </div>
-      ) : (
-        <>
-          {providers &&
-            Object.values(providers).map((provider) => {
-              return (
+            <Link href="/profile">
+              <Image
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className="h-11 w-11 rounded-full border-[#2b6cb0] border-2 p-0.5"
+                alt="profile-image"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
                 <button
                   type="button"
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className="text-black btn border-2 border-black"
                 >
-                  Sign In
+                  Sign in
                 </button>
-              );
-            })}
-        </>
-      )}
+              ))}
+          </>
+        )}
+      </div>
 
       {/* Mobile View */}
-      <div className="flex md:hidden items-center">
+      <div className="sm:hidden flex relative">
         {session?.user ? (
-          <div className="relative ">
+          <div className="flex">
             <Image
               src={session?.user.image}
-              alt="main-logo"
-              width={50}
-              height={50}
-              className="h-11 w-11 rounded-full border-[#2b6cb0] border-2 p-0.5 "
-              onClick={() => {
-                setToggleDropdown((prev) => !prev);
-              }}
+              width={37}
+              height={37}
+              className="h-11 w-11 rounded-full border-[#2b6cb0] border-2 p-0.5"
+              alt="profile"
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
+
             {toggleDropdown && (
-              <div className="dropdown_menu" style={{ zIndex: 100 }}>
+              <div
+                className="dropdown_menu"
+                style={{
+                  zIndex: 100,
+                }}
+              >
                 <Link
                   href="/profile"
-                  onClick={() => {
-                    setToggleDropdown(false);
-                  }}
+                  onClick={() => setToggleDropdown(false)}
                   className="para_text"
                 >
                   My Profile
                 </Link>
                 <Link
                   href="/create-prompt"
-                  onClick={() => {
-                    setToggleDropdown(false);
-                  }}
                   className="para_text"
+                  onClick={() => setToggleDropdown(false)}
                 >
                   Create Prompt
                 </Link>
@@ -130,18 +132,18 @@ const Navbar = () => {
         ) : (
           <>
             {providers &&
-              Object.values(providers).map((provider) => {
-                return (
-                  <button
-                    type="button"
-                    key={provider.name}
-                    onClick={() => signIn(provider.id)}
-                    className="text-black btn border-2 border-black"
-                  >
-                    Sign In
-                  </button>
-                );
-              })}
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="text-black btn border-2 border-black"
+                >
+                  Sign in
+                </button>
+              ))}
           </>
         )}
       </div>
